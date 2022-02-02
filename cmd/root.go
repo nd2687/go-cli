85291d3418ce74b619f453f49370e809dc1512d6
nd2687 <kawano.fanta@gmail.com>
2022-02-02 23:54:49 +0900

    Change rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,8 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+  "fmt"
 )
 
 
@@ -24,7 +26,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("hello cobra!")
+  },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
